b560908e6d7e5d0e1fbe101cf792406c1a006f02
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2021-10-20 14:44:15 +0200

    added square hypotenuse exercise

--- 005_triangle/triangle/main.cpp
+++ 005_triangle/triangle/main.cpp
@@ -1,14 +1,50 @@
-// Creare un programma in c++ che chieda all置tente di immettere la lunghezza (intera) di due cateti di
-// un triangolo rettangolo e stampi a terminale il quadrato dell段potenusa.
+/* Obiettivo:
+    Creare un programma in c++ che chieda all置tente di immettere la lunghezza (intera) di due cateti di
+    un triangolo rettangolo e stampi a terminale il quadrato dell段potenusa.
+ */
+
 #include <iostream>
+#include <cmath>
+
+#define N 2
 
 using namespace std;
 
+int inputLeg(int);
+bool isValid(int);
+
+const char* ORD[N] = { "FIRST", "SECOND" };
+
 int main() {
+    int c1 = 0, c2 = 0, square_hypo = 0;
 
+    cout << "This program calculates, using the length of two (integeres) legs, the square of the hypotenuse of a right triangle ..." << endl;
+    cout << "==========================" << endl;
 
+    c1 = inputLeg(0);
+    c2 = inputLeg(1);
 
-
+    square_hypo = pow(c1, 2) + pow(c2, 2);
+    cout << "==========================" << endl;
+    cout << "The square of the hypotenuse is (" << square_hypo << ")" << endl;
 
     return 0;
+}
+
+int inputLeg(int index) {
+    int num;
+    bool valid = true;
+    do {
+        cout << "Insert the length of the " << ORD[index] << " LEG (must be an integer): ";
+        cin >> num;
+        if (!isValid(num)) {
+            valid = false;
+            cout << "ERR - input value not valid. Must be a positive integer (> 0)" << endl;
+        } else valid = true;
+    } while (!valid);
+    return num;
+}
+
+bool isValid(int num) {
+    return num > 0;
 }
